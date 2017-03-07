4ab4b019fd9bf292628316c760b6406be8a2dea7
Jan Galinski <jan.galinski@holisticon.de>
2017-03-07 23:06:13 +0100

    enable support for typed variables, fixes #38

--- src/main/java/org/camunda/bpm/extension/mockito/delegate/VariableScopeFake.java
+++ src/main/java/org/camunda/bpm/extension/mockito/delegate/VariableScopeFake.java
@@ -1,107 +1,45 @@
 package org.camunda.bpm.extension.mockito.delegate;
 
-
 import org.camunda.bpm.engine.delegate.VariableScope;
-import org.camunda.bpm.engine.variable.VariableMap;
-import org.camunda.bpm.engine.variable.Variables;
-import org.camunda.bpm.engine.variable.value.TypedValue;
+import org.camunda.bpm.engine.impl.core.variable.CoreVariableInstance;
+import org.camunda.bpm.engine.impl.core.variable.scope.AbstractVariableScope;
+import org.camunda.bpm.engine.impl.core.variable.scope.SimpleVariableInstance;
+import org.camunda.bpm.engine.impl.core.variable.scope.VariableInstanceFactory;
+import org.camunda.bpm.engine.impl.core.variable.scope.VariableInstanceLifecycleListener;
+import org.camunda.bpm.engine.impl.core.variable.scope.VariableStore;
 
-import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-public class VariableScopeFake<T extends VariableScopeFake> implements VariableScope {
+public class VariableScopeFake<T extends VariableScopeFake> extends AbstractVariableScope implements VariableScope {
 
-  private final VariableMap global = Variables.createVariables();
-  private final VariableMap local = Variables.createVariables();
-  private final String variableScopeKey;
+  protected VariableStore<CoreVariableInstance> variableStore = new VariableStore<>();
+  protected VariableInstanceFactory<CoreVariableInstance> variableInstanceFactory = (name, value, isTransient) -> new SimpleVariableInstance(name, value);
 
-  public VariableScopeFake() {
-    this("fake");
+  @Override
+  protected VariableStore<CoreVariableInstance> getVariableStore() {
+    return variableStore;
   }
 
-  public VariableScopeFake(String variableScopeKey) {
-    this.variableScopeKey = variableScopeKey;
+  @Override
+  protected VariableInstanceFactory<CoreVariableInstance> getVariableInstanceFactory() {
+    return variableInstanceFactory;
   }
 
   @Override
   public String getVariableScopeKey() {
-    return variableScopeKey;
+    return "fake";
   }
 
   @Override
-  public Map<String, Object> getVariables() {
-    return global;
+  protected List<VariableInstanceLifecycleListener<CoreVariableInstance>> getVariableInstanceLifecycleListeners() {
+    return Collections.EMPTY_LIST;
   }
 
   @Override
-  public VariableMap getVariablesTyped() {
-    return global;
-  }
-
-  @Override
-  public VariableMap getVariablesTyped(boolean deserializeValues) {
-    throw new UnsupportedOperationException("variables-typed not implemented");
-  }
-
-  @Override
-  public Map<String, Object> getVariablesLocal() {
-    return local;
-  }
-
-  @Override
-  public VariableMap getVariablesLocalTyped() {
-    return local;
-  }
-
-  @Override
-  public VariableMap getVariablesLocalTyped(boolean deserializeValues) {
-    throw new UnsupportedOperationException("variables-typed not implemented");
-  }
-
-  @Override
-  public Object getVariable(String variableName) {
-    return global.get(variableName);
-  }
-
-  @Override
-  public Object getVariableLocal(String variableName) {
-    return local.get(variableName);
-  }
-
-  @Override
-  public <T extends TypedValue> T getVariableTyped(String variableName) {
-    return global.getValueTyped(variableName);
-  }
-
-  @Override
-  public <T extends TypedValue> T getVariableTyped(String variableName, boolean deserializeValue) {
-    throw new UnsupportedOperationException("variables-typed not implemented");
-  }
-
-  @Override
-  public <T extends TypedValue> T getVariableLocalTyped(String variableName) {
-    return local.getValueTyped(variableName);
-  }
-
-  @Override
-  public <T extends TypedValue> T getVariableLocalTyped(String variableName, boolean deserializeValue) {
-    throw new UnsupportedOperationException("variables-typed not implemented");
-  }
-
-  @Override
-  public Set<String> getVariableNames() {
-    return global.keySet();
-  }
-
-  @Override
-  public Set<String> getVariableNamesLocal() {
-    return local.keySet();
-  }
-
-  @Override
-  public void setVariable(String variableName, Object value) {
-    global.putValue(variableName, value);
+  public AbstractVariableScope getParentVariableScope() {
+    return null;
   }
 
   public T withVariable(String variableName, Object value) {
@@ -109,20 +47,9 @@ public class VariableScopeFake<T extends VariableScopeFake> implements VariableS
     return (T) this;
   }
 
-
-  @Override
-  public void setVariableLocal(String variableName, Object value) {
-    local.putValue(variableName, value);
-  }
-
   public T withVariableLocal(String variableName, Object value) {
     setVariableLocal(variableName, value);
     return (T) this;
-  }
-
-  @Override
-  public void setVariables(Map<String, ? extends Object> variables) {
-    global.putAll(variables);
   }
 
   public T withVariables(Map<String, ? extends Object> variables) {
@@ -130,63 +57,8 @@ public class VariableScopeFake<T extends VariableScopeFake> implements VariableS
     return (T) this;
   }
 
-  @Override
-  public void setVariablesLocal(Map<String, ? extends Object> variables) {
-    local.putAll(variables);
-  }
-
   public T withVariablesLocal(Map<String, ? extends Object> variables) {
     setVariablesLocal(variables);
     return (T) this;
-  }
-
-  @Override
-  public boolean hasVariables() {
-    return !global.isEmpty();
-  }
-
-  @Override
-  public boolean hasVariablesLocal() {
-    return !local.isEmpty();
-  }
-
-  @Override
-  public boolean hasVariable(String variableName) {
-    return global.containsKey(variableName);
-  }
-
-  @Override
-  public boolean hasVariableLocal(String variableName) {
-    return local.containsKey(variableName);
-  }
-
-  @Override
-  public void removeVariable(String variableName) {
-    global.remove(variableName);
-  }
-
-  @Override
-  public void removeVariableLocal(String variableName) {
-    local.remove(variableName);
-  }
-
-  @Override
-  public void removeVariables(Collection<String> variableNames) {
-    variableNames.forEach(this::removeVariable);
-  }
-
-  @Override
-  public void removeVariablesLocal(Collection<String> variableNames) {
-    variableNames.forEach(this::removeVariableLocal);
-  }
-
-  @Override
-  public void removeVariables() {
-    global.clear();
-  }
-
-  @Override
-  public void removeVariablesLocal() {
-    local.clear();
   }
 }
--- src/test/java/org/camunda/bpm/extension/mockito/delegate/VariableScopeFakeTest.java
+++ src/test/java/org/camunda/bpm/extension/mockito/delegate/VariableScopeFakeTest.java
@@ -1,11 +1,14 @@
 package org.camunda.bpm.extension.mockito.delegate;
 
 
+import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.StringValue;
 import org.camunda.bpm.extension.mockito.CamundaMockito;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.camunda.bpm.engine.variable.Variables.stringValue;
 
 public class VariableScopeFakeTest {
 
@@ -16,5 +19,27 @@ public class VariableScopeFakeTest {
     variableScope.withVariable("foo", 1).withVariables(Variables.putValue("bar", 2));
 
     assertThat(variableScope.getVariableNames()).containsOnly("foo", "bar");
+  }
+
+  @Test
+  public void variablesTyped() throws Exception {
+    VariableMap variables = Variables.putValueTyped("foo", stringValue("bar"));
+
+    variableScope.setVariablesLocal(variables);
+
+    StringValue foo = variableScope.getVariableLocalTyped("foo");
+
+    assertThat(foo.getValue()).isEqualTo("bar");
+  }
+
+  @Test
+  public void variablesLocal() throws Exception {
+    VariableMap variables = Variables.putValue("foo", "bar");
+    variableScope.setVariablesLocal(variables);
+
+    String foo = (String) variableScope.getVariableLocal("foo");
+
+    assertThat(foo).isEqualTo("bar");
+
   }
 }
