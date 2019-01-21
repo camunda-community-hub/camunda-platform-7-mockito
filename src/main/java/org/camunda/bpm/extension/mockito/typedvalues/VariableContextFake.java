916839bfcc39143fc853224c9cb6a7139fa6351a
Jan Galinski <jan.galinski@holisticon.de>
2019-01-21 22:27:46 +0100

    introduce variablecontextFake, fixes #74

---
+++ src/main/java/org/camunda/bpm/extension/mockito/typedvalues/VariableContextFake.java
@@ -0,0 +1,44 @@
+package org.camunda.bpm.extension.mockito.typedvalues;
+
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.context.VariableContext;
+import org.camunda.bpm.engine.variable.value.TypedValue;
+
+import java.util.Set;
+import java.util.function.Supplier;
+
+/**
+ * Implementation of {@link VariableContext} that internally uses a {@link VariableMap} to keep key/value pairs.
+ * <p>
+ * Use {@link VariableContextFake#add(String, TypedValue)} for fluent building.
+ */
+public class VariableContextFake implements VariableContext, Supplier<VariableMap> {
+
+  private final VariableMap typedValues = Variables.createVariables();
+
+  @Override
+  public TypedValue resolve(final String key) {
+    return typedValues.getValueTyped(key);
+  }
+
+  @Override
+  public boolean containsVariable(final String key) {
+    return typedValues.containsKey(key);
+  }
+
+  @Override
+  public Set<String> keySet() {
+    return typedValues.keySet();
+  }
+
+  public VariableContextFake add(String key, TypedValue value) {
+    typedValues.put(key, value);
+    return this;
+  }
+
+  @Override
+  public VariableMap get() {
+    return typedValues;
+  }
+}
---
+++ src/test/java/org/camunda/bpm/extension/mockito/typedvalues/VariableContextFakeTest.java
@@ -0,0 +1,37 @@
+package org.camunda.bpm.extension.mockito.typedvalues;
+
+
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VariableContextFakeTest {
+
+  private final VariableContextFake fake = new VariableContextFake();
+
+  @Test
+  public void can_add_to_fake() {
+    assertThat(fake.keySet()).isEmpty();
+    assertThat(fake.containsVariable("foo")).isFalse();
+    assertThat(fake.resolve("foo")).isNull();
+
+    fake.add("foo", Variables.booleanValue(false));
+
+    assertThat(fake.keySet()).containsOnly("foo");
+    assertThat(fake.containsVariable("foo")).isTrue();
+    assertThat(fake.resolve("foo")).isEqualTo(Variables.booleanValue(false));
+  }
+
+  @Test
+  public void to_variableMap() {
+    fake.add("bar", Variables.stringValue("foo"));
+    fake.add("foo", Variables.stringValue("bar"));
+
+    VariableMap map = fake.get();
+
+    assertThat(map.getValue("foo", String.class)).isEqualTo("bar");
+    assertThat(map.getValue("bar", String.class)).isEqualTo("foo");
+  }
+}
